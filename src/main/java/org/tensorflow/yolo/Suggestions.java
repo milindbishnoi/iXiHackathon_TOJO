242e1ab4652bd5591995e16b99153f3bfaf74694
milindbishnoi <milind.curio@gmail.com>
2020-02-05 01:32:56 +0530

    Styles added

--- src/main/java/org/tensorflow/yolo/Suggestions.java
+++ src/main/java/org/tensorflow/yolo/Suggestions.java
@@ -4,13 +4,26 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Suggestions extends Activity {
 
+    TextView YT;
+    TextView Time;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_suggestions);
-        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=w77zPAtVTuI")));
+        YT= (TextView) findViewById(R.id.knowmore);
+        Time=(TextView) findViewById(R.id.time); 
+        YT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=w77zPAtVTuI")));
+            }
+        });
+
+
     }
 }
