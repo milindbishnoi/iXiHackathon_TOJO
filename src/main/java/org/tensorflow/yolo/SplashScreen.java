ef63de63f307dd92940ac0110c7cb5033ad53ecb
milindbishnoi <milind.curio@gmail.com>
2020-02-05 02:34:11 +0530

    Bugs removed

--- src/main/java/org/tensorflow/yolo/SplashScreen.java
+++ src/main/java/org/tensorflow/yolo/SplashScreen.java
@@ -1,5 +1,6 @@
 package org.tensorflow.yolo;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
@@ -7,7 +8,7 @@ import android.os.Bundle;
 
 import org.tensorflow.yolo.view.ClassifierActivity;
 
-public class SplashScreen extends AppCompatActivity {
+public class SplashScreen extends Activity {
 
     private final int SPLASH_DISPLAY_LENGTH = 1000;
     @Override
@@ -25,5 +26,6 @@ public class SplashScreen extends AppCompatActivity {
                 SplashScreen.this.finish();
             }
         }, SPLASH_DISPLAY_LENGTH);
+
     }
 }
