720f8f3c5d314a0c39b11e724f4c28158c1ce115
milindbishnoi <milind.curio@gmail.com>
2020-02-04 22:39:54 +0530

    suggestion added

--- src/main/java/org/tensorflow/yolo/Suggestions.java
+++ src/main/java/org/tensorflow/yolo/Suggestions.java
@@ -1,6 +1,8 @@
 package org.tensorflow.yolo;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 public class Suggestions extends Activity {
@@ -9,5 +11,6 @@ public class Suggestions extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_suggestions);
+        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=w77zPAtVTuI")));
     }
 }
